9485e0fa90923a3f40883031fab0a645901dd54c
mohamed othman <mohamedothmanmoutayb@gmail.com>
2024-09-29 20:46:46 +0100

    changing the content in the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Hamed.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Mohamed Othman.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Toronto, Canada. I&#39;m
+          I&#39;m a software engineer based in Agadir, Morocco. I&#39;m
           passionate about learning new technologies and sharing knowledge with
           others.
         </p>
