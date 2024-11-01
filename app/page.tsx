fafef31ae663837992b581917b4994b78093cb32
mohamed othman <mohamedothmanmoutayb@gmail.com>
2024-11-01 11:00:59 +0100

    adding the posts section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Intro from '@/components/intro'
+import RecentPosts from '@/components/recent-posts'
 
 const Home = () => {
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
+        <RecentPosts />
       </div>
     </section>
   )
